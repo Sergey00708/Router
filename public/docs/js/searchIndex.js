5cee19196d2a750683bde22a6b9a429072e302ce
Sergey <Fenix00708@gmail.com>
2022-06-22 16:16:17 +0300

    homework12

---
+++ public/docs/js/searchIndex.js
@@ -0,0 +1,39 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Sergey\\Router\\Router",
+            "name": "Router",
+            "summary": "\u041A\u043B\u0430\u0441\u0441\u0020\u0440\u043E\u0443\u0442\u0435\u0440,\u0020\u0432\u044B\u043F\u043E\u043B\u043D\u044F\u0435\u0442\u0020\u043F\u043E\u0434\u043A\u043B\u044E\u0447\u0435\u043D\u0438\u0435\u0020\u0441\u0442\u0440\u0430\u043D\u0438\u0446",
+            "url": "classes/Sergey-Router-Router.html"
+        },                {
+            "fqsen": "\\Sergey\\Router\\Router\u003A\u003AaddRoute\u0028\u0029",
+            "name": "addRoute",
+            "summary": "",
+            "url": "classes/Sergey-Router-Router.html#method_addRoute"
+        },                {
+            "fqsen": "\\Sergey\\Router\\Router\u003A\u003Aroute\u0028\u0029",
+            "name": "route",
+            "summary": "",
+            "url": "classes/Sergey-Router-Router.html#method_route"
+        },                {
+            "fqsen": "\\Sergey\\Router\\Router\u003A\u003A\u0024routes",
+            "name": "routes",
+            "summary": "",
+            "url": "classes/Sergey-Router-Router.html#property_routes"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\Sergey\\Router",
+            "name": "Router",
+            "summary": "",
+            "url": "namespaces/sergey-router.html"
+        },                {
+            "fqsen": "\\Sergey",
+            "name": "Sergey",
+            "summary": "",
+            "url": "namespaces/sergey.html"
+        }            ]
+);
--- src/Router.php
+++ src/Router.php
@@ -1,21 +1,40 @@
 <?php
+/**
+ * Файл роутера
+ * @version 1.0
+ */
 
 namespace Sergey\Router;
 
 use Sergey\Framework\Interfaces\RouteInterface;
 
-
+/**
+ * Класс роутер, выполняет подключение страниц
+ * @author Sergey00708
+ * @package Router
+ */
 class Router implements RouteInterface
 {
-
+    /**
+     * @var array
+     */
     public $routes = [];
 
+    /**
+     * @param string $path
+     * @param $action
+     * @return void
+     */
     public function addRoute(string $path, $action)
     {
         $path = trim($path, '/');
         $this->routes[$path] = $action;
     }
 
+    /**
+     * @param string $uri
+     * @return callable
+     */
     public function route(string $uri): callable
     {
         $uri = trim($uri, '/');
